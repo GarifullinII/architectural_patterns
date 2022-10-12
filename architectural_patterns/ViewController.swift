fe477a7dcacda3058a2d1ca3a47b11e0f05f2715
GArifullinII <ildaridrisovich@gmail.com>
2022-10-12 18:33:04 +0400

    Initial Commit

---
+++ architectural_patterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  architectural_patterns
+//
+//  Created by Ildar Garifullin on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
